c15273101d507c861c31084016a6980b369febe0
hqcoder05 <hoangquocfw@gmail.com>
2026-08-19 18:28:09 +0700

    Initial commit

---
+++ src/main/java/com/booking_ticket_platform/GeekUpApplication.java
@@ -0,0 +1,13 @@
+package com.booking_ticket_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekUpApplication.class, args);
+	}
+
+}
